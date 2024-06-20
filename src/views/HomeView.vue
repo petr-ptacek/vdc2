378b0eaf4173e8cd087f39b406893e2ace2e2378
Petr Ptacek <petr.ptacek99@gmail.com>
2024-06-20 22:22:16 +0200

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,13 +4,12 @@ import AppHeader from "@/components/AppHeader.vue";
 </script>
 
 <template>
-  <div class="h-screen w-full flex flex-col">
-    <AppHeader class="bg-green-500 basis-[60px]" />
-    <div class="flex-1 bg-yellow-500">
-      <main class="container mx-auto">
-        <button class="text-yellow">faf</button>
-      </main>
-    </div>
-    <AppFooter class="bg-red-500" />
-  </div>
+	<div class="h-screen w-full flex flex-col">
+		<AppHeader class="flex-shrink-0 flex-grow-0" />
+		<div class="flex-1 bg-yellow">
+			<main class="container mx-auto">
+			</main>
+		</div>
+		<AppFooter />
+	</div>
 </template>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,50 +1,53 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}"
-  ],
-  theme: {
-    colors: {
-      white: "#fff",
-      black: "#fff",
+	content: [
+		"./index.html",
+		"./src/**/*.{vue,js,ts,jsx,tsx}"
+	],
+	theme: {
+		colors: {
+			white: "#fff",
+			black: "#fff",
 
-      primary: "#343C43",
-      secondary: "#AF6545",
-      tertiary: "#4C8362",
+			primary: "#343C43",
+			secondary: "#AF6545",
+			tertiary: "#4C8362",
 
-      green: {
-        DEFAULT: "#4C8362",
-        semi: "#60B766",
-        light: "#edf3ef"
-      },
-      red: {
-        DEFAULT: "#FF1A1A",
-        light: "#ffe8e8"
-      },
-      brown: {
-        DEFAULT: "#EADABE",
-        dark: "#AF6545",
-        semi: "#EEE2CC",
-        light: "#f7f0e4",
-        hover: "#a57560"
-      },
-      gray: {
-        DEFAULT: "#D9D9D9",
-        dark: "#999999",
-        black: "#232C34",
-        light: "#F6F6F6",
+			green: {
+				DEFAULT: "#4C8362",
+				semi: "#60B766",
+				light: "#edf3ef"
+			},
+			red: {
+				DEFAULT: "#FF1A1A",
+				light: "#ffe8e8"
+			},
+			brown: {
+				DEFAULT: "#EADABE",
+				dark: "#AF6545",
+				semi: "#EEE2CC",
+				light: "#f7f0e4",
+				hover: "#a57560",
 
-        text: "#9A9DA1",
-        hover: "#949393"
-      },
-      yellow: {
-        DEFAULT: "#cfa204",
-        light: "#faf6e6"
-      }
-    },
-    extend: {}
-  },
-  plugins: []
+				input: "#FAF6EF",
+				inputText: "#A19685"
+			},
+			gray: {
+				DEFAULT: "#D9D9D9",
+				dark: "#999999",
+				black: "#232C34",
+				light: "#F6F6F6",
+
+				text: "#9A9DA1",
+				hover: "#949393"
+			},
+			yellow: {
+				DEFAULT: "#cfa204",
+				light: "#faf6e6"
+			}
+		},
+		extend: {}
+	},
+	plugins: []
 } satisfies Config;
