763f6686f6de034c3fd4fc28bcf46fcdaf1fc5b4
Petr Ptacek <petr.ptacek@olc.cz>
2024-07-01 10:03:25 +0200

    add favicon

--- typed-svg-icons.d.ts
+++ typed-svg-icons.d.ts
@@ -5,6 +5,7 @@ declare interface _SVGIcons_ {
 	"bars-3": "bars-3";
 	"bars-4": "bars-4";
 	"cursor-arrow-rays": "cursor-arrow-rays";
+	"digeocat-lib": "digeocat-lib";
 	"document-text": "document-text";
 	"logo-eu": "logo-eu";
 	"logo-university-palacky": "logo-university-palacky";
