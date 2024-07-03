12fbda8233ff138d2912e5a04d351d4a83b7ec71
Petr Ptacek <petr.ptacek99@gmail.com>
2024-07-03 22:03:48 +0200

    update

--- src/views/home/components/DrawerExcerpts/components/TheToolbar.vue
+++ src/views/home/components/DrawerExcerpts/components/TheToolbar.vue
@@ -23,6 +23,8 @@ const selectOptions: User[] = [
 ];
 
 const selectedUser = ref(null);
+
+const extendedOptionsOpened = ref(false);
 </script>
 
 <template>
@@ -44,8 +46,29 @@ const selectedUser = ref(null);
 		</div>
 
 		<div>
-			<WDrawerContainer dir="top-to-bottom" size="8rem">
-				<div class="flex h-full items-center justify-center gap-4">
+			<div class="h-[0rem] shadow-[0_2px_4px] shadow-primary bg-brown-semi relative">
+				<button
+					class="transition-transform rounded-full w-fit h-fit absolute text-secondary left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-10"
+					:class="{
+						'rotate-180': extendedOptionsOpened
+					}"
+					@click="extendedOptionsOpened = !extendedOptionsOpened"
+				>
+					<WIcon
+						name="chevron-down"
+						size="2xl"
+					/>
+				</button>
+			</div>
+
+			<WDrawerContainer
+				disable-resizer-line
+				disable-open-button
+				:opened="extendedOptionsOpened"
+				dir="top-to-bottom"
+				size="6rem"
+			>
+				<div class="flex h-full items-end justify-center gap-4">
 					<button class="btn btn--secondary btn--circle w-fit h-fit">
 						<WIcon name="printer" size="md" />
 					</button>
