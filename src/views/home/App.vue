a434bd714cde004b6a57e3de271c070163bddbeb
Petr Ptacek <petr.ptacek99@gmail.com>
2024-07-10 21:36:51 +0200

    update

--- src/views/home/App.vue
+++ src/views/home/App.vue
@@ -27,141 +27,149 @@ const appStore = useAppStore();
 				dir="left-to-right"
 				v-model:opened="appStore.drawerForms.opened"
 			>
-				<div class="h-full overflow-auto">
-					<div class="space-y-2 py-2 px-4">
-						<WAccordion>
-							<template #title>
-								<div class="font-bold">Obecné</div>
-							</template>
+				<div class="h-full">
+					<div class="flex h-full flex-col">
+						<div class="flex-1 overflow-auto">
+							<div class="py-4 px-4 space-y-2">
+								<WAccordion>
+									<template #title>
+										<div class="font-bold">Obecné</div>
+									</template>
 
-							<div class="p-1.5">
-								<div class="flex flex-col gap-1.5">
-									<div>
-										<label for="multiselect-tags-autor" class="uppercase text-[1.2rem]">
-											<span class="">Autor</span>
-										</label>
-										<WMultiselectTags
-											:config="{
+									<div class="p-1.5">
+										<div class="flex flex-col gap-1.5">
+											<div>
+												<label for="multiselect-tags-autor" class="uppercase text-[1.2rem]">
+													<span class="">Autor</span>
+												</label>
+												<WMultiselectTags
+													:config="{
 												id: 'multiselect-tags-autor',
 												placeholder: 'Autor'
 											}"
-										/>
-									</div>
+												/>
+											</div>
 
-									<div>
-										<label for="multiselect-tags-travelogue" class="uppercase text-[1.2rem]">
-											<span class="">Cestopis</span>
-										</label>
+											<div>
+												<label for="multiselect-tags-travelogue" class="uppercase text-[1.2rem]">
+													<span class="">Cestopis</span>
+												</label>
 
-										<WMultiselectTags
-											:config="{
+												<WMultiselectTags
+													:config="{
 												id: 'multiselect-tags-travelogue',
 												placeholder: 'Cestopis'
 											}"
-										/>
-									</div>
+												/>
+											</div>
 
-									<div>
-										<label for="multiselect-tags-area" class="uppercase text-[1.2rem]">
-											<span class="">Oblast</span>
-										</label>
+											<div>
+												<label for="multiselect-tags-area" class="uppercase text-[1.2rem]">
+													<span class="">Oblast</span>
+												</label>
 
-										<WMultiselectTags
-											:config="{
+												<WMultiselectTags
+													:config="{
 												id: 'multiselect-tags-area',
 												placeholder: 'Oblast'
 											}"
-										/>
-									</div>
+												/>
+											</div>
 
-									<div>
-										<label for="multiselect-tags-thema" class="uppercase text-[1.2rem]">
-											<span class="">Téma</span>
-										</label>
+											<div>
+												<label for="multiselect-tags-thema" class="uppercase text-[1.2rem]">
+													<span class="">Téma</span>
+												</label>
 
-										<WMultiselectTags
-											:config="{
+												<WMultiselectTags
+													:config="{
 												id: 'multiselect-tags-thema',
 												placeholder: 'Téma'
 											}"
-										/>
-									</div>
+												/>
+											</div>
 
-									<div>
-										<label for="multiselect-tags-transport" class="uppercase text-[1.2rem]">
-											<span class="">Doprava</span>
-										</label>
+											<div>
+												<label for="multiselect-tags-transport" class="uppercase text-[1.2rem]">
+													<span class="">Doprava</span>
+												</label>
 
-										<WMultiselectTags
-											:config="{
+												<WMultiselectTags
+													:config="{
 												id: 'multiselect-tags-transport',
 												placeholder: 'Doprava'
 											}"
-										/>
+												/>
+											</div>
+										</div>
 									</div>
-								</div>
+								</WAccordion>
+
+								<WAccordion>
+									<template #title>
+										<div class="font-bold">Datum cesty</div>
+									</template>
+
+									<div class="p-1.5">
+										<div class="flex flex-col gap-1.5">
+											<div class="flex flex-col gap-1.5">
+												<div class="text-gray uppercase">Od</div>
+												<ComplexDatePicker />
+											</div>
+
+											<div class="flex flex-col gap-1.5">
+												<div class="text-gray uppercase">Do</div>
+												<ComplexDatePicker />
+											</div>
+										</div>
+									</div>
+								</WAccordion>
+
+								<WAccordion>
+									<template #title>
+										<div class="font-bold">Datum sepsání</div>
+									</template>
+
+									<div class="p-1.5">
+										<div class="flex flex-col gap-1.5">
+											<div class="flex flex-col gap-1.5">
+												<div class="text-gray uppercase">Od</div>
+												<ComplexDatePicker />
+											</div>
+
+											<div class="flex flex-col gap-1.5">
+												<div class="text-gray uppercase">Do</div>
+												<ComplexDatePicker />
+											</div>
+										</div>
+									</div>
+								</WAccordion>
+
+								<WAccordion>
+									<template #title>
+										<div class="font-bold">Datum vydání</div>
+									</template>
+
+									<div class="p-1.5">
+										<div class="flex flex-col gap-1.5">
+											<div class="flex flex-col gap-1.5">
+												<div class="text-gray uppercase">Od</div>
+												<ComplexDatePicker />
+											</div>
+
+											<div class="flex flex-col gap-1.5">
+												<div class="text-gray uppercase">Do</div>
+												<ComplexDatePicker />
+											</div>
+										</div>
+									</div>
+								</WAccordion>
 							</div>
-						</WAccordion>
+						</div>
 
-						<WAccordion>
-							<template #title>
-								<div class="font-bold">Datum cesty</div>
-							</template>
-
-							<div class="p-1.5">
-								<div class="flex flex-col gap-1.5">
-									<div class="flex flex-col gap-1.5">
-										<div class="text-gray uppercase">Od</div>
-										<ComplexDatePicker />
-									</div>
-
-									<div class="flex flex-col gap-1.5">
-										<div class="text-gray uppercase">Do</div>
-										<ComplexDatePicker />
-									</div>
-								</div>
-							</div>
-						</WAccordion>
-
-						<WAccordion>
-							<template #title>
-								<div class="font-bold">Datum sepsání</div>
-							</template>
-
-							<div class="p-1.5">
-								<div class="flex flex-col gap-1.5">
-									<div class="flex flex-col gap-1.5">
-										<div class="text-gray uppercase">Od</div>
-										<ComplexDatePicker />
-									</div>
-
-									<div class="flex flex-col gap-1.5">
-										<div class="text-gray uppercase">Do</div>
-										<ComplexDatePicker />
-									</div>
-								</div>
-							</div>
-						</WAccordion>
-
-						<WAccordion>
-							<template #title>
-								<div class="font-bold">Datum vydání</div>
-							</template>
-
-							<div class="p-1.5">
-								<div class="flex flex-col gap-1.5">
-									<div class="flex flex-col gap-1.5">
-										<div class="text-gray uppercase">Od</div>
-										<ComplexDatePicker />
-									</div>
-
-									<div class="flex flex-col gap-1.5">
-										<div class="text-gray uppercase">Do</div>
-										<ComplexDatePicker />
-									</div>
-								</div>
-							</div>
-						</WAccordion>
+						<div class="justify-self-end py-4 flex justify-center">
+							<button class="btn btn--tertiary">Hledat</button>
+						</div>
 					</div>
 				</div>
 			</WDrawerContainer>
