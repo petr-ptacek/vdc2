19d670dc3ae806a2512e515fefa79867488fb933
Petr Ptacek <petr.ptacek99@gmail.com>
2024-07-10 22:33:10 +0200

    update

--- src/views/home/components/ExcerptDetail/tabs/MapInfo.vue
+++ src/views/home/components/ExcerptDetail/tabs/MapInfo.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
+import { WLeafletMap } from "@/components";
 </script>
 
 <template>
-	<div class="flex h-full justify-center items-center">
-		Map Info
+	<div class="w-full h-full">
+		<WLeafletMap
+			class="w-full h-full z-0"
+			:center="[35.917973, 14.409943]"
+			:zoom="5"
+		/>
 	</div>
 </template>
